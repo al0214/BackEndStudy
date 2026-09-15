74e71b77bf591fbc8631093f7b814a2b9876a314
al0214 <sisang951@gmail.com>
2026-09-15 18:16:20 +0900

    Feat : First Commit

---
+++ fastapiStudy/src/main02.py
@@ -0,0 +1,39 @@
+import uvicorn
+from fastapi import FastAPI, Query
+from typing import List, Dict
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id:int):
+    return {"item_id" : item_id}
+
+@app.post("/items/")
+def create_item(item: dict):
+    return {"item": item}
+
+@app.get("/items/")
+def read_items(skip: int = 0, limit: int = 10):
+    return {
+        "skip": skip, "limit": limit
+    }
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: dict):
+    return {
+        "item_id": item_id,
+        "updated_item": item
+    }
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int):
+    return {
+        "message": f"Item {item_id} has been deleted"
+    }
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
---
+++ fastapiStudy/src/main03.py
@@ -0,0 +1,44 @@
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from typing import List
+
+app = FastAPI()
+
+# class Item(BaseModel):
+#     name: str
+#     description: Optional[str] = None
+#     price: float
+#     tax: float = 0.1
+
+class Item(BaseModel):
+    name: str = Field(...,
+        title="Item Name",
+        min_length=2,
+        max_length=50
+    )
+
+    description: str | None = Field(
+        None,
+        description="The descriprion of the item",
+        max_length=300
+    )
+
+    price: float = Field(...,
+        gt = 0,
+        description="The price must be greater than zero"
+    )
+
+    tag: List[str] = Field(
+        default=[],
+        alias='item-tags'
+    )
+
+@app.post("/items/")
+def create_item(item: Item):
+    return {
+        "item" : item.model_dump()
+    }
+
+if __name__ == "__main__":
+    uvicorn.run("main03:app", host="0.0.0.0", port=8000, reload=True)
